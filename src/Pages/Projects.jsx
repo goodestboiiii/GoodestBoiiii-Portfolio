413314310106cc22e54208c4e5134ea4f6e210c3
ZeeCreate <jeanzey.romano@gmail.com>
2025-11-19 03:51:44 +0800

    Aligned title and divider of each pages

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -15,17 +15,16 @@ const Projects = () => {
 
         <div className="projects-top">
           <div className="project-card">
-            <a href= "https://www.figma.com/design/Y9HeXRHVEW5sb3id6oyqHt/Test-V.-Create-a-Prototype-in-Figma?node-id=0-1&t=ZCRqz88CoI5SQaXq-1" target = "_blank"><img src={proj2} alt="Project 2" className="project-img" /></a>
+            <a href= "https://www.figma.com/design/Y9HeXRHVEW5sb3id6oyqHt/Test-V.-Create-a-Prototype-in-Figma?node-id=0-1&t=ZCRqz88CoI5SQaXq-1" target = "_blank"> <img src={proj2} alt="Project 2" className="project-img" /></a>
             <div className="project-tags">
-              <span>UI/UX Design</span>
-              <span>Figma</span>
+              <span>Mob App Dev</span>
+              <span>Python</span>
             </div>
           </div>
-        </div>
+        
 
-        {/* Projects bottom row (1 column) */}
-        <div className="projects-bottom">
-          <div className="project-card project-large">
+   
+          <div className="project-card">
             <a href= "https://www.figma.com/design/6LrCjA3aBRdR1nSk6yE1gq/Techlympic-Contest?node-id=0-1&t=EsNGCnHOd0OkZZJb-1" target="_blank" ><img src={proj3} alt="Project 3" className="project-img" /></a>
             <div className="project-tags">
               <span>UI/UX Design</span>
